56105d08730fc5dfa222205b1457fd72f7b39122
MpenduloN <133219249+MpenduloN@users.noreply.github.com>
2025-11-17 22:06:31 +0200

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -13,7 +13,7 @@ export default defineConfig({
 
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
+import App from "./App";
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
