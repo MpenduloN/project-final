a48f0c28e1271bd739260f9afe31bcb360b224e7
MpenduloN <133219249+MpenduloN@users.noreply.github.com>
2025-11-17 20:57:13 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { AuthProvider, useAuth } from "./contexts/AuthContext";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
