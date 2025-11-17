68a03e79db22f09ba66554b8f896176b50c695c2
MpenduloN <133219249+MpenduloN@users.noreply.github.com>
2025-11-17 21:19:11 +0200

    Update App.tsx

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { AuthProvider, useAuth } from './contexts/AuthContext';
+import { AuthProvider, useAuth } from "./contexts/AuthContext";
 import { Auth } from './components/Auth';
 import { Layout } from './components/Layout';
 import { Dashboard } from './components/Dashboard';
