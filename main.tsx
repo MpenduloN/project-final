a8619cdb012e1d6f40d527b0d2214640e1b45db5
MpenduloN <133219249+MpenduloN@users.noreply.github.com>
2025-11-17 22:33:23 +0200

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,23 +1,10 @@
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
-
-
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import './index.css';
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
